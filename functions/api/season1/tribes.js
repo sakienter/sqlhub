5d9ee79ba5a5826e03d79a3a356ee13a30a756be
sakienter <23mm0402@moon.aichi-u.ac.jp>
2026-06-11 22:37:43 +0900

    Read S1 tribe config from KV

--- functions/api/season1/tribes.js
+++ functions/api/season1/tribes.js
@@ -1,5 +1,35 @@
-export async function onRequestGet() {
-  return new Response(JSON.stringify({ DAY1: {}, DAY2: {}, DAY3: {}, DAY4: {} }), {
-    headers: { 'Content-Type': 'application/json; charset=utf-8' }
+const DEFAULT_TRIBE_CONFIG = {
+  DAY1: {},
+  DAY2: {},
+  DAY3: {},
+  DAY4: {}
+};
+
+export async function onRequestGet(context) {
+  const config = await readConfig(context);
+
+  return jsonResponse(config, 200, {
+    'Cache-Control': 'no-store'
+  });
+}
+
+async function readConfig(context) {
+  const kv = context.env.TRIBE_CONFIG;
+
+  if (!kv) {
+    return DEFAULT_TRIBE_CONFIG;
+  }
+
+  const data = await kv.get('season2-tribes', 'json');
+  return data || DEFAULT_TRIBE_CONFIG;
+}
+
+function jsonResponse(data, status, extraHeaders = {}) {
+  return new Response(JSON.stringify(data), {
+    status,
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      ...extraHeaders
+    }
   });
 }
